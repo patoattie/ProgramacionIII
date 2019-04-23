521a14f68ce6ff84b063973a003502e9425da9f2
patoattie <patoattie@gmail.com>
2019-04-23 19:20:59 -0300

    Clase 1

---
+++ clase1/UNO.php
@@ -0,0 +1,11 @@
+<?php
+	$dato = "Hola";
+	//require "DOS.php";
+	//require "DOS.php";
+	//include "DOS.php";
+	//include "DOS.php";
+	require_once "DOS.php";
+	require_once "DOS.php";
+	//include_once "DOS.php";
+	//include_once "DOS.php";
+?>
