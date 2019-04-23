841e7ea136148f4b975bde397aa3a6d6af421d5f
patoattie <patoattie@gmail.com>
2019-04-23 18:42:35 -0300

    Clase 1

---
+++ clase1/ejer2.php
@@ -0,0 +1,8 @@
+<?php
+	/*Hacer un programa en PHP que sume el contenido de dos variables $x = -3 y $y = 15. Mostrar
+	el resultado final.*/
+
+	$x = -3;
+	$y = 15;
+	echo "$x + $y = ".($x+$y);
+?>
---
+++ clase1/ejer3.php
@@ -0,0 +1,10 @@
+<?php
+	/*Partiendo del ejercicio anterior, modificar la salida por pantalla para que se visualice el valor de
+	la variable $x, el valor de la variable $y y el resultado final en líneas distintas (recordar que el
+	salto de línea en HTML es la etiqueta <br/>).*/
+
+	$x = -3;
+	$y = 15;
+	echo '$x = '.$x.'<br/>$y = '.$y.'<br/>$x + $y = '.($x+$y);
+//	echo '$x = '.$x."$x + $y = ".($x+$y);
+?>
