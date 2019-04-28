9df4f73343aa006c6030542c83224690b05d4d70
patoattie <patoattie@gmail.com>
2019-04-28 11:56:48 -0300

    Ejercicios pendientes clase 1

--- clase1/ejer12.php
+++ clase1/ejer12.php
@@ -3,5 +3,15 @@
 	contenga como elementos: ‘color’, ‘marca’, ‘trazo’ y ‘precio’. Crear, cargar y mostrar tres
 	lapiceras.*/
 
-	
+	define("CANTIDAD", 3);
+	$color = "";
+	$marca = "";
+	$trazo = "";
+	$precio = (float)0;
+	$lapicera = array('color' => $color, 'marca' => $marca, 'trazo' => $trazo, 'precio' => $precio);
+
+	for($i = 0; $i < CANTIDAD; $i++)
+	{ 
+		$color = 
+	}
 ?>
