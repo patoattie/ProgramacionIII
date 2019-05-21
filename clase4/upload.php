f4c2378b09dbebc1ba7a84735a66a4f4e51fd673
patoattie <patoattie@gmail.com>
2019-05-21 18:48:31 -0300

    Clase 4

--- clase4/upload.php
+++ clase4/upload.php
@@ -75,6 +75,9 @@ if ($uploadOk === FALSE) {
 	//MUEVO EL ARCHIVO DEL TEMPORAL AL DESTINO FINAL
     if (move_uploaded_file($_FILES["archivo"]["tmp_name"], $destino)) {
         //echo "<br/>El archivo ". basename( $_FILES["archivo"]["name"]). " ha sido subido exitosamente.";
+        //Agrego la marca de agua
+        ManejadorArchivos::agregarMarcaAgua($destino, "MarcaDeAgua/fotoDos.png");
+
         echo "<br/>El archivo " . pathinfo($destino, PATHINFO_BASENAME) . " ha sido subido exitosamente.";
     } else {
         echo "<br/>Lamentablemente ocurri&oacute; un error y no se pudo subir el archivo.";
