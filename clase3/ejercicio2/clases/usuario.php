0aa6648b786f9ad5b8f0595e63e726933c333ab2
Patricio Attie <patoattie@gmail.com>
2019-05-21 14:08:02 -0300

    Ejercicios clase 3

---
+++ clase3/ejercicio2/clases/usuario.php
@@ -0,0 +1,49 @@
+<?php
+
+class Usuario
+{
+	private $id;
+	private $email;
+	private $clave;
+	private $alias;
+	private $fecha;
+	private static $proximoId;
+
+	public function __construct($email, $clave, $alias, $id = 0, $fecha = "")
+	{
+		$this->email = $email;
+		$this->clave = $clave;
+		$this->alias = $alias;
+
+		if ($id === 0)
+		{
+			$this->id = 
+		}
+		else
+		{
+			$this->id = $id;
+		}
+
+		if ($fecha === "")
+		{
+			$this->fecha = date("d/m/Y h:i:s");
+		}
+		else
+		{
+			$this->fecha = $fecha;
+		}
+	}
+
+	public function leerUsuarios()
+	{
+		$archivo = fopen("archivos/usuarios.txt", "r");
+		$arrayUsuarios = array();
+
+		while (!feof($archivo))
+		{
+			$usuario = new Usuario()
+		}
+	}
+}
+
+?>
