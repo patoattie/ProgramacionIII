55823067acfab65f3f817baf115cc86b07aa2bea
Patricio Attie <patoattie@gmail.com>
2019-06-26 14:19:21 -0300

    ORM

--- practicaORM/src/app/models/ORM/cd.php
+++ practicaORM/src/app/models/ORM/cd.php
@@ -25,6 +25,99 @@ class cd extends \Illuminate\Database\Eloquent\Model
 	{
 		return (new cd())->where($condicion)->get();
 	}
+
+	//carga los valores de los atributos contenidos en el array pasado por parámetro
+	public function setParams($parametros)
+	{
+        //cargo los atributos a ingresar en el objeto
+        foreach ($parametros as $key => $value)
+        {
+            $this->setAttribute($key, $value);
+        }
+	}
+
+	public function setID($id)
+	{
+		$this->setAttribute($this->getCampoID(), $id);
+	}
+
+	public function getID()
+	{
+		return $this->getAttribute($this->getCampoID());
+	}
+
+	public function getCampoID()
+	{
+		return $this->getKeyName();
+	}
+
+	public function calculaID()
+	{
+		return $this->getIncrementing();
+	}
+
+	public function insertar()
+	{
+        $respuesta = 0; //OK
+
+     	if($this->calculaID()) //El ID es autoincremental, lo dejo en nulo para que lo calcule la BD.
+     	{
+     		$this->setID(null);
+     		$this->save();
+     	}
+     	else
+     	{
+            //retorna true si dentro de los parámetros ingresados está el ID. Util para cuando el ID en la BD no es autoincremental y se lo tengo que pasar.
+            $id = $this->getID();
+
+     		if($id) //Como no es autoincremental el ID, valido que se haya ingresado en el body.
+     		{
+     			if($this->find($id)) //Si existe el ID, muestro mensaje al usuario y no ingreso nada
+     			{
+					$respuesta = -1; //"El CD ya se encuentra ingresado"
+     			}
+     			else
+     			{
+		     		$this->save();
+     			}
+     		}
+     		else
+     		{
+				$respuesta = -2; //"Falta pasar el parametro con el ID del CD a cargar"
+     		}
+     	}
+
+     	return $respuesta;
+	}
+
+	public function modificar()
+	{
+        $respuesta = 0; //OK
+
+        //retorna true si dentro de los parámetros ingresados está el ID. Util para cuando el ID en la BD no es autoincremental y se lo tengo que pasar.
+        $id = $this->getID();
+
+        if($id) //tengo el ID ingresado dentro de los parámetros del body
+        {
+            //retorno un objeto con el ID solicitado.
+            //$unCD = $unCD->find($id);
+
+            if(!$this->find($id)) //Si NO existe el ID, muestro mensaje al usuario y no modifico nada
+            {
+                $respuesta = -1; //"El CD no existe"
+            }
+            else
+            {
+                $this->save();
+            }
+        }
+        else
+        {
+            $respuesta = -2; //"Falta pasar el parametro con el ID del CD a modificar"
+        }
+
+        return $respuesta;
+	}
 }
 
 ?>
--- practicaORM/src/app/models/ORM/cdControler.php
+++ practicaORM/src/app/models/ORM/cdControler.php
@@ -18,9 +18,21 @@ class cdControler implements IApiControler
 		<ul>
 			<li>(GET)listarCDs: muestra todos los CDs cargados.</li>
 			<li>(GET)listarCD: muestra todos los CDs cargados que satisfagan los parámetros ingresados.</li>
-            <li>(POST)guardarCD: agrega el CD a la BD de acuerdo a los parámetros ingresados. Si la BD soporta ID autoincremental, el mismo se calcula, sino se solicita su ingreso por parámetro.</li>
-            <li>(PUT)guardarCD: modifica un CD en la BD de acuerdo a los parámetros ingresados. Busca por parámetro ID, si no se lo proporciona retorna error.</li>
-			<li>(DELETE)borrarCD: borra un CD en la BD de acuerdo al ID ingresado. Si no se lo proporciona retorna error.</li>
+            <li>(POST)guardarCD: agrega el CD a la BD de acuerdo a los parámetros ingresados. Si la BD soporta ID autoincremental, el mismo se calcula, sino se solicita su ingreso por parámetro.<br>
+                Estados devueltos:<br>
+                ( 0) -> Insert OK<br>
+                (-1) -> El CD ya está ingresado en la colección<br>
+                (-2) -> Falta informar el parámetro ID</li>
+            <li>(PUT)guardarCD: modifica un CD en la BD de acuerdo a los parámetros ingresados. Busca por parámetro ID, si no se lo proporciona retorna error.<br>
+                Estados devueltos:<br>
+                ( 0) -> Update OK<br>
+                (-1) -> El CD no existe en la colección<br>
+                (-2) -> Falta informar el parámetro ID</li>
+			<li>(DELETE)borrarCD: borra un CD en la BD de acuerdo al ID ingresado. Si no se lo proporciona retorna error.<br>
+                Estados devueltos:<br>
+                ( 0) -> Delete OK<br>
+                (-1) -> El CD no existe en la colección<br>
+                (-2) -> Falta informar el parámetro ID</li>
 		</ul>");
     }
     
@@ -46,60 +58,25 @@ class cdControler implements IApiControler
     public function CargarUno($request, $response, $args) {
      	 //complete el codigo
 
-		$respuesta = 0; //OK
-
         $condicion = self::cargarConBody($request);
 
         //cargo un objeto de tipo cd con los parametros ingresados por POST
         $unCD = new cd();
 
         //cargo los atributos a ingresar en el objeto
-        foreach ($condicion as $key => $value)
-        {
-            $unCD->setAttribute($key, $value);
-        }
+        $unCD->setParams($condicion);
 
-     	if($unCD->getIncrementing()) //El ID es autoincremental, lo dejo en nulo para que lo calcule la BD.
-     	{
-     		$unCD->setAttribute($unCD->getKeyName(), null);
-     		$unCD->save();
-     		$newResponse = $response->withJson($respuesta, 200);  //"CD ingresado a la coleccion"
-     	}
-     	else
-     	{
-            //retorna true si dentro de los parámetros ingresados está el ID. Util para cuando el ID en la BD no es autoincremental y se lo tengo que pasar.
-            $tengoClave = array_key_exists($unCD->getKeyName(), $condicion);
+        //inserto en la base
+        $estado = $unCD->insertar();
 
-     		if($tengoClave) //Como no es autoincremental el ID, valido que se haya ingresado en el body.
-     		{
-                //valor del ID
-                $id = $unCD[$unCD->getKeyName()];
-
-     			if($unCD->find($id)) //Si existe el ID, muestro mensaje al usuario y no ingreso nada
-     			{
-					$respuesta = -1; //"El CD ya se encuentra ingresado"
-     				$newResponse = $response->withJson($respuesta, 200);  
-     			}
-     			else
-     			{
-		     		$unCD->save();
-		     		$newResponse = $response->withJson($respuesta, 200);  //"CD ingresado a la coleccion"
-     			}
-     		}
-     		else
-     		{
-				$respuesta = -2; //"Falta pasar el parametro con el ID del CD a cargar"
-				$newResponse = $response->withJson($respuesta, 200);  
-     		}
-     	}
+        //Devuelvo el estado
+        $newResponse = $response->withJson($estado, 200);
 
         return $newResponse;
     }
 
     public function BorrarUno($request, $response, $args) {
   		//complete el codigo
-
-        $respuesta = 0; //OK
 
         //cargo un objeto de tipo cd con los parametros ingresados por DELETE
         $unCD = new cd();
@@ -139,46 +116,17 @@ class cdControler implements IApiControler
     public function ModificarUno($request, $response, $args) {
      	//complete el codigo
 
-        $respuesta = 0; //OK
-
         //cargo un objeto de tipo cd con los parametros ingresados por PUT
         $unCD = new cd();
         $condicion = self::cargarConBody($request);
 
-        //retorna true si dentro de los parámetros ingresados está el ID. Util para cuando el ID en la BD no es autoincremental y se lo tengo que pasar.
-        $tengoClave = array_key_exists($unCD->getKeyName(), $condicion);
+        //cargo los atributos a ingresar en el objeto
+        $unCD->setParams($condicion);
 
-        if($tengoClave) //tengo el ID ingresado dentro de los parámetros del body
-        {
-            //valor del ID
-            $id = $condicion[$unCD->getKeyName()];
+        //inserto en la base
+        $estado = $unCD->modificar();
 
-            //retorno un objeto con el ID solicitado.
-            $unCD = $unCD->find($id);
-
-            if(!$unCD) //Si NO existe el ID, muestro mensaje al usuario y no modifico nada
-            {
-                $respuesta = -1; //"El CD no existe"
-                $newResponse = $response->withJson($respuesta, 200);  
-            }
-            else
-            {
-                //cargo los atributos a modificar en el objeto
-                foreach ($condicion as $key => $value)
-                {
-                    $unCD->setAttribute($key, $value);
-                }
-
-                $unCD->save();
-                $newResponse = $response->withJson($respuesta, 200);  //"CD modificado"
-            }
-        }
-        else
-        {
-            $respuesta = -2; //"Falta pasar el parametro con el ID del CD a modificar"
-            $newResponse = $response->withJson($respuesta, 200);  
-        }
-
+        $newResponse = $response->withJson($estado, 200);  
         return $newResponse;
     }
 
